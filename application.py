a0c79fb37214b22d84b2bba5f7ee97b81a3ce2b4
SANJUKUMARI98 <sanju080598@gmail.com>
2023-07-30 17:03:44 +0530

    second commit

---
+++ application.py
@@ -0,0 +1,51 @@
+from flask import Flask,request,app,render_template
+from flask import Response
+import pickle
+import numpy as np
+import pandas as pd
+
+
+application = Flask(__name__)
+app=application
+
+model= pickle.load(open('Model/decisiontreeRegressorModel','rb'))
+
+# route for homepage
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for single data point prediction
+@app.route('/predictdata',methods= ['GET','POST'])
+def predict_datapoint():
+    result=""
+
+    if request.method=='POST':
+        result=""
+
+        if request.method=='POST':
+            MedInc=float(request.form.get('MedInc'))
+            HouseAge=float(request.form.get('HouseAge'))
+            AveRooms=float(request.form.get('AveRooms'))
+            AveBedrms=float(request.form.get('AveBedrms'))
+            Population=float(request.form.get('Population'))
+            AveOccup=float(request.form.get('AveOccup'))
+            Latitude=float(request.form.get('Latitude'))
+            Longitude=float(request.form.get('Longitude'))
+
+            predict = model.predict(new_data)
+
+            return render_template('singlePrediction.html',result=result)
+
+        else:
+            return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")           
+
+
+
+
